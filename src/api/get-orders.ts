bd676e6eab647075bec0f9871df479daae52a954
Hugo Silva <hugo.marcelo@clevertech.biz>
2024-01-22 20:23:03 -0300

    feat: connect pagination to API

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,9 @@
 import { api } from '@/lib/axios'
 
+export interface GetOrdersQuery {
+  pageIndex?: number | null
+}
+
 export interface GetOrdersResponse {
   orders: {
     orderId: string
@@ -10,15 +14,15 @@ export interface GetOrdersResponse {
   }[]
   meta: {
     pageIndex: number
-    pageSize: number
+    perPage: number
     totalCount: number
   }
 }
 
-export async function getOrders() {
+export async function getOrders({ pageIndex }: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
-      pageIndex: 0,
+      pageIndex,
     },
   })
 
